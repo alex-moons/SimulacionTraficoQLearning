683a1549ddd5861cf563010f783419a9a2bd8b3f
alex <a01721287@itesm.mx>
2022-08-09 16:16:40 -0500

    cambios 1

--- Activida 01/main.cpp
+++ Activida 01/main.cpp
@@ -1,8 +1,18 @@
 #include <string>
 #include <iostream>
+#include <regex>
 
 using namespace std;
 
 int main(){
-    cout<<"hello world"<<endl;
+    int nDesigners;
+    string pName,desc;
+
+    cin>>nDesigners;
+    cin>>pName;
+    
+    for (int i = 0; i < nDesigners; i++){
+        cin>>desc;
+    }
+
 }
